6bbd4a1e6e1d28c1a750edee72ba19ee4c063bed
José Manuel <Jos Manuel@DESKTOP-L67G7N4>
2023-09-17 23:27:10 -0500

    Agregar archivos de proyecto.

---
+++ lab5 caso2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5_caso2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la base: ");
+            int numB = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el exponente: ");
+            int exponente = int.Parse(Console.ReadLine());
+
+            int resultado = 1;
+
+            for (int i = 1; i <= exponente; i++) 
+            {
+                resultado = resultado * numB;
+            }
+            Console.Write("La potencia es: "+resultado);
+            Console.ReadKey();
+        }
+    }
+}
